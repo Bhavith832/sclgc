84630a911ac2622184ca2ea30891b5682212a2ba
Bhavith832 <bhavithnhce07@gmail.com>
2024-12-25 22:46:15 +0530

    Update pgm1.py

--- pgm1.py
+++ pgm1.py
@@ -1,1 +1,8 @@
-Hi...
+first_number=(input("Enter the name:"))
+second_number=(input("Enter the name:"))
+height1=float(input(f"Enter {first_number} number:"))
+heigth2=float(input(f"Enter {second_number} number:"))
+if height1 > heigth2:
+    print(height1,f"{first_number} is Greater.")
+else:
+    print(heigth2,f"{second_number} is Greater.")
