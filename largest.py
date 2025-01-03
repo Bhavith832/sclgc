d3bec611a8aac5eb0c324279cf6f9c5b6445ee3c
Bhavith832 <bhavithnhce07@gmail.com>
2025-01-03 18:25:40 +0530

    largest

---
+++ largest.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter the first number:"))
+num2 = int(input("enter the seocnd number:"))
+num3 = int(input("enter the third number:"))
+
+if num1 > num2:
+    print(num1,)
+else:
+    print(num3,"is the largest number.")
---
+++ percentage.py
@@ -0,0 +1,11 @@
+percent = int(input("Enter the percentage:"))
+if percent >90 and percent <100:
+    print("the grade is A.")
+elif percent >80 and percent <90:
+    print("the grade is B.")
+elif percent >70 and percent <80:
+    print("the grade is C")
+elif percent >60 and percent <70:
+    print("The grade is D.")
+else:
+    print("the grade is F.")
