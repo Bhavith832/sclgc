e59714e53ffb76cf45749d1a5683bd27c575a2f8
Bhavith832 <bhavithnhce07@gmail.com>
2025-01-03 19:10:45 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,6 @@
+user = str(input("enter the character:")).lower()
+lst = ['a','e','i','o','u']
+if user in lst:
+    print(user,"The Entered charater is vowel.")
+else:
+    print(user,"The Entered character is consonent.")
