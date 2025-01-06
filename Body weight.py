c944d210f7cc158924d96ba03b614b0e58cdbe80
Bhavith832 <bhavithnhce07@gmail.com>
2025-01-06 18:10:52 +0530

    Create Body weight.py

---
+++ Body weight.py
@@ -0,0 +1,10 @@
+BMI = int(input("Enter the weight:"))
+
+if BMI <18.5:
+    print("You are under weight.")
+elif BMI <= 18.5 and BMI < 24.9:
+    print("you are in normal weight:")
+elif BMI >= 25 and BMI < 29.9:
+    print("You are in overweight.")
+elif BMI >= 30:
+    print("You are obese.")
