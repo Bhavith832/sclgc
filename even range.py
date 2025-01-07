44e80dff49878abb56088e14692dd08671dc94ad
Bhavith832 <bhavithnhce07@gmail.com>
2025-01-07 13:49:09 +0530

    Create even range.py

---
+++ even range.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number:"))
+for num in range(10 and 30):
+    if num % 2 == 0:
+        print(num,"The given number is even.")
+    else:
+        print(num,"The given number is odd")
+        
