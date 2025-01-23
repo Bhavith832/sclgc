571318dfa5a5433dec9e02a8997feac7445993bc
Bhavith832 <bhavithnhce07@gmail.com>
2025-01-23 20:40:49 +0530

    mul

--- multiplication.py
+++ multiplication.py
@@ -1,8 +1,9 @@
 for num in range(1,21):
     print()
     print(f"table of {num}:")
+    print()
   
     for i in range(1,11):
          print(f"{num} x {i} = {num * i}")
-        #  print()
+
     
