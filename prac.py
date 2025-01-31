e699c758bc7fa418acc61536956eb215358f5ebb
Bhavith832 <bhavithnhce07@gmail.com>
2025-01-31 20:07:24 +0530

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,9 +1,49 @@
-user_input=input("Enter the user name:")
-plan=input("Enter the plan EX: Normal, prime?")
-id=int(input("Enter the id:"))
-lst=[111,222,333]
+class Messenger:
+    def use_keyboard(self):
+        print("You are using keyboard.")
+    def send_message(self):
+        print("You have send message.")
+    def receive_message(self):
+        print("You have received message.")
+        print()
 
-if plan=="prime" and id in lst:
-    print("ur a prime number")
-else:
-    print("ur not prime number")
+class whatsapp(Messenger):
+      def send_message(self):
+        print("You have send message.")
+      def receive_message(self):
+        print("You have received message.")
+        print()
+
+class facebook(Messenger):
+      def send_message(self):
+        print("You have send message.")
+      def receive_message(self):
+        print("You have received message.")
+        print()
+
+class instagram(Messenger):
+      def send_message(self):
+        print("You have send message.")
+      def receive_message(self):
+        print("You have received message.")
+        print()
+
+m=Messenger()
+m.use_keyboard()
+m.send_message()
+m.receive_message()
+
+w=whatsapp()
+w.use_keyboard()
+w.send_message()
+w.receive_message()
+
+f=facebook()
+f.use_keyboard()
+f.send_message()
+f.receive_message()
+
+i=instagram()
+i.use_keyboard()
+i.send_message()
+i.receive_message()
