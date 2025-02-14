a7be49ec706a6e2bc1b643d8e68513d3ad51f5e4
Bhavith832 <bhavithnhce07@gmail.com>
2025-02-14 14:25:58 +0530

    Update prac.py

--- prac.py
+++ prac.py
@@ -1,49 +1,62 @@
-class Messenger:
-    def use_keyboard(self):
-        print("You are using keyboard.")
-    def send_message(self):
-        print("You have send message.")
-    def receive_message(self):
-        print("You have received message.")
-        print()
+# class Messenger:
+#     def use_keyboard(self):
+#         print("You are using keyboard.")
+#     def send_message(self):
+#         print("You have send message.")
+#     def receive_message(self):
+#         print("You have received message.")
+#         print()
 
-class whatsapp(Messenger):
-      def send_message(self):
-        print("You have send message.")
-      def receive_message(self):
-        print("You have received message.")
-        print()
+# class whatsapp(Messenger):
+#       def send_message(self):
+#         print("You have send message.")
+#       def receive_message(self):
+#         print("You have received message.")
+#         print()
 
-class facebook(Messenger):
-      def send_message(self):
-        print("You have send message.")
-      def receive_message(self):
-        print("You have received message.")
-        print()
+# class facebook(Messenger):
+#       def send_message(self):
+#         print("You have send message.")
+#       def receive_message(self):
+#         print("You have received message.")
+#         print()
 
-class instagram(Messenger):
-      def send_message(self):
-        print("You have send message.")
-      def receive_message(self):
-        print("You have received message.")
-        print()
+# class instagram(Messenger):
+#       def send_message(self):
+#         print("You have send message.")
+#       def receive_message(self):
+#         print("You have received message.")
+#         print()
 
-m=Messenger()
-m.use_keyboard()
-m.send_message()
-m.receive_message()
+# m=Messenger()
+# m.use_keyboard()
+# m.send_message()
+# m.receive_message()
 
-w=whatsapp()
-w.use_keyboard()
-w.send_message()
-w.receive_message()
+# w=whatsapp()
+# w.use_keyboard()
+# w.send_message()
+# w.receive_message()
 
-f=facebook()
-f.use_keyboard()
-f.send_message()
-f.receive_message()
+# f=facebook()
+# f.use_keyboard()
+# f.send_message()
+# f.receive_message()
 
-i=instagram()
-i.use_keyboard()
-i.send_message()
-i.receive_message()
+# i=instagram()
+# i.use_keyboard()
+# i.send_message()
+# i.receive_message()
+# print("Enter the number:")
+# num1 = int(input())
+# print(num1)
+# a=2.5
+# b=2.5
+# print(a*b)
+# a=2
+# b=5
+# a,b=b,a
+# print("a is:",a,"b is:",b)
+# a = "110"
+# b = "011"
+# print(bin(int(a,2)+int(b,2))[2:])
