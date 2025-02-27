a0e163c84addc26a72c59b488d2fa7460624334a
Bhavith832 <bhavithnhce07@gmail.com>
2025-02-27 17:52:44 +0530

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,2 +1,6 @@
-number = [1,2,3,4,5]
-print(sum(number))
+def remove_duplicates(lst):
+    return list(dict.fromkeys(lst))  # Uses dict to maintain order and remove duplicates
+
+# Example usage
+lst = [1, 2, 3, 4, 3, 2, 5, 6, 5, 6, 7]
+print(remove_duplicates(lst))  # Output: [1, 2, 3, 4, 5, 6, 7]
