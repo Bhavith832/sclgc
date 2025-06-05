37a17320b844124a25950cffc272a950d8199585
Bhavith832 <bhavithnhce07@gmail.com>
2025-06-05 17:00:31 +0530

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,10 +1,4 @@
-lst1 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
-lst2 = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
-for i in lst1:
-    if i % 2 == 0:
-        print(i)
-print("odd numbers.")
-
-for i in lst2:
-    if i % 2 != 0:
-        print(i)
+for i in range(1,21):
+    print(f"Multiplication of table {i}:")
+    for j in range(1,11):
+        print(f"{i} X {j} = {i*j}")
